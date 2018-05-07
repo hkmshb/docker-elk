36a470f4cda5d3d52e02c77b34a7b6e10a2b022e
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-05-07 15:51:07 +0100

    Correct column names and rename sql script file (smlsettlementareas)

--- logstash/sql/fe_smlsettlementareas.sql
+++ logstash/sql/fe_smlsettlementareas.sql
@@ -4,7 +4,8 @@ SELECT
      fe.building_assessment_inhabited,
      fe.building_assessment_confidence,
      fe.building_assessment_date,
-     fe.settlement_id, fe.settlement_name,
+     fe.settlementid as "settlement_id",
+     fe.settlementname as "settlement_name",
      fe.settlementobjectid as "settlement_objectid",
      fe.settlementnamecalc as "settlement_name_calc",
      fe.majorroaddistance as "major_road_distance",
@@ -18,7 +19,7 @@ SELECT
      bl.statecode as "state_code",
      bl.statename as "state_name",
      fe.weight, fe.nbdenominator, fe.source
-FROM nigeria_master_trunk_rev_head.fe_smlsettlement fe
+FROM nigeria_master_trunk_rev_head.fe_smlsettlementareas fe
 JOIN nigeria_master_trunk_rev_head.boundary_vaccwards bw
   ON (fe.wardcode = bw.wardcode)
 JOIN nigeria_master_trunk_rev_head.boundary_vacclgas bl
