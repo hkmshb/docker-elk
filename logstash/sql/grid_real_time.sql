e9e71afc2879a698ceaebad1f6870d3b20b1f195
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-05-17 15:45:20 +0100

    Add grid_real_time pipeline conf

---
+++ logstash/sql/grid_real_time.sql
@@ -0,0 +1,22 @@
+SELECT
+    sw.id, sw.created_by,
+    sw.url, sw.survey_url,
+    sw.attachments_url, sw.created,
+    sw.survey, sw.form_id,
+    sw.lga, lg.lganame,
+    sw.instanceid, sw.ward,
+    wd.wardname,
+    sw.note1, sw.note6,
+    sw.state, st.statename,
+    sw.dc_name, sw.endtime,
+    sw.comments, sw.deviceid,
+    sw.poi_name, sw.poi_type,
+    sw.starttime, sw.point_type,
+    sw.latitude, sw.longitude
+FROM public.grid_sw_form1 sw
+LEFT JOIN nigeria_master_trunk_rev_head.boundary_vaccwards wd
+       ON (wd.wardcode = sw.ward)
+LEFT JOIN nigeria_master_trunk_rev_head.boundary_vacclgas lg
+       ON (lg.lgacode = wd.lgacode)
+LEFT JOIN nigeria_master_trunk_rev_head.boundary_vaccstates st
+       ON (st.statecode = lg.statecode)
